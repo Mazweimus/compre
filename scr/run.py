7da88d02be05c20e6e8e0ba3f33597cfb5eee487
Ma2mus <barcama24@sps-prosek.cz>
2026-02-13 11:09:07 +0100

    prepare Counter and added run output text for tracking output

--- scr/run.py
+++ scr/run.py
@@ -3,7 +3,8 @@ with open("temporary-data/readme.html", "rb") as file:
     lines = file.readlines()
     la = 0
     for i in lines:
-        # if la == 1:
-        #     break
-        print(i)
+        if la == 1:
+            break
+        coutner = Counter(i)
+        print(coutner)
         la = la + 1
