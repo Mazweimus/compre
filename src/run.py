45b895bc55d78624c46c20518aa66105e6bc4941
Mazweimus <matty.barcal@gmail.com>
2026-02-27 23:22:26 +0100

    ADD: structure of the code by printing it

--- src/run.py
+++ src/run.py
@@ -2,7 +2,39 @@ import heapq
 from collections import Counter
 import os
 
+ 
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
 
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+def buildTree(frequency):
+    heap = []
+    for char, freq in frequency.items():
+        newNode = Node(char, freq)
+        heap.append(newNode)
+    heapq.heapify(heap)
+    while len(heap) > 1:
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+        connection = Node(None, left.freq + right.freq)
+        connection.left = left
+        connection.right = right
+        heapq.heappush(heap, connection)
+    return(heap[0])
+
+def print_codes(node, current_code=""):
+    if node is not None:
+        if node.char is not None:
+            print(f"Byte: {node.char:<3} | Kód: {current_code}")
+            
+        print_codes(node.left, current_code + "0")
+        print_codes(node.right, current_code + "1")
 
 print("Welcome to Compre\n\nFor showing all comands type \"help\"\n")
 commands = {
@@ -11,30 +43,21 @@ commands = {
 }
 
 while True:
-    inta=input(str(""))
-    if (inta == "quit" or inta == "q"):
+    userInput = input(str("")).lower()
+    if (userInput == "quit" or userInput == "q"):
         break
-    print("\n" + inta + "\n")
+    elif (userInput == "help"):
+        for cmd, info in commands.items():
+            print(cmd + " > " + info)
+    elif userInput.startswith("compre"):
+        with open("temporary-data/11987.jpg", "rb") as file:
+            lines = file.read()
+            shtm = Counter(lines)
+            print("GENERATING CODE...")
+            tree = buildTree(shtm)
+            print_codes(tree)
+    else:
+        print("\n" + userInput + "\n")
    
-# class Node:
-#     def __init__(self, char, freq):
-#         self.char = char
-#         self.freq = freq
-#         self.left = None
-#         self.right = None
 
-#     def __lt__(self, other):
-#         return self.freq < other.freq
-
-# def buildTree(frequency):
-#     heap = []
-#     for char, freq in frequency.items():
-#         newNode = Node(char, freq)
-#         newNode.append(heap)
-#     heapq.heapify(heap)
-#     while len(heap) > 1:
-#         left = heapq.heappop()
-#         right = heapq.heappop()
-#         connection = Node(None, left.freq + right.freq)
-#         print(left)
 
