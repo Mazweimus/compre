442eb1eae19f55ef61fbb090b2cb009974cb769e
Matěj Barcal <barcama24@sps-prosek.lad>
2026-05-27 13:12:13 +0200

    refactor pickle to normal bytes and change createCompressFile() function

--- src/lib/lib_file.py
+++ src/lib/lib_file.py
@@ -47,11 +47,14 @@ def createCompressFile(route:str, save_route_status=False) -> None:
         else:
             saveRoute = osPath + "/" + fileName + ".barcal"
         with open(saveRoute, "wb") as huf:
-            pickle.dump(shtm, huf)
-            pickle.dump(startOfTheFile, huf)
-            pickle.dump(endOfTheFile, huf)
+            CounterDataObject = lib_tree.createCouterBytes(shtm)
+            nameOfTheFileBytes = lib_tree.turnFileNameToBytes(startOfTheFile, endOfTheFile)
+            huf.write(CounterDataObject[0]) #Number of 2 bytes, that is used to know, how many cycles the compress algorithm does
+            huf.write(nameOfTheFileBytes[0]) #name and path of the file
+            huf.write(nameOfTheFileBytes[1]) # end of the file .
             huf.write(addedBufferMultiplier.to_bytes(1, byteorder='big'))
             huf.write(output_bytes)
+            huf.write(CounterDataObject[1])
         print("Hotovo! Soubor naleznete v data adresáři")
 
 def createNormalFile(route:str) -> None:
--- src/lib/lib_tree.py
+++ src/lib/lib_tree.py
@@ -41,16 +41,19 @@ def build_Huff_Tree(node:Node, current_code:str="", code_dict:None=None) -> Node
     build_Huff_Tree(node.right, current_code + "1", code_dict)
     return code_dict
 
-def createCouterBytes(counterData : Counter) -> object:
+def createCouterBytes(counterData : Counter) -> list[int, bytes]:
     """Recreate Counter object into bytes due to secure storing"""
     CounterTree = bytes()
     counter = 0
     for char, freq in counterData.items():
+        print(char)
         CounterTree += char.to_bytes(2, byteorder="big")
         CounterTree += freq.to_bytes(2, byteorder="big")
         counter += 1
-    return {counter, counterData}
-def turnFileNameToBytes(startOfTheFile:str, endOfTheFile:str) -> object:
+    return [counter, CounterTree]
+def turnFileNameToBytes(startOfTheFile:str, endOfTheFile:str) -> list[str]:
     """Create folder location in bytes due to secure storing"""
-    ...
+    newStartOfTheFile = startOfTheFile.to_bytes(2, byteorder="big")
+    newEndOfTheFile = endOfTheFile.to_bytes(2, byteorder="big")
+    return [newStartOfTheFile, newEndOfTheFile]
     
